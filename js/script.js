f917ac577e71b36d1670541e5f051b67eaa81cac
Rassolenko Dmitriy <rassoldima@yandex.ru>
2020-10-12 04:32:08 +0300

    lesson02

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-let money; 
-let income;
-let addExpenses; 
-let deposit; 
-let mission; 
-let period;
+let money = 50000; 
+let income = 'фриланс';
+let addExpenses = 'Интернет, Такси, Коммуналка'; 
+let deposit = true; 
+let mission = 150000; 
+let period = 5;
+let budgetDay = money / 30;
 
-console.log('Первый урок');
-alert('Hello World!');
+console.log(money, income, deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев'); 
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLocaleLowerCase().split(','));
+console.log(budgetDay);
